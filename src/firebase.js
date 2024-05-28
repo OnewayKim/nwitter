8e816b933c9fcaad5ec21172812b4005f41c34f8
oneway <oneway@codemind.co.kr>
2024-05-28 17:09:41 +0900

    02. 환경 설정하기

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXBJ99ZCJMyJT5HpQD2tlxs2m6a5AOv0Y",
+  authDomain: "nwitter-3aff7.firebaseapp.com",
+  projectId: "nwitter-3aff7",
+  storageBucket: "nwitter-3aff7.appspot.com",
+  messagingSenderId: "466776824025",
+  appId: "1:466776824025:web:9080e0d25e827413a286c8"
+};
+
+export default initializeApp(firebaseConfig);
