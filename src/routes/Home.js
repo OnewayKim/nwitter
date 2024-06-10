e8f0d1cc8ec2118dff5f49f4dbba6e56e46430a5
oneway <oneway@codemind.co.kr>
2024-06-10 15:47:42 +0900

    05. 트위터? 아니, '누이터' 핵심 기능 만들기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,19 @@
 import { dbService } from 'fbase';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import Nweet from "components/Nweet";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
 
-    const getNweets = async () => {
-        const dbNweets = await getDocs(collection(dbService, "nweets"));
-        dbNweets.forEach((document) => {
-            const nweetObject = { ...document.data(), id: document.id };
-            setNweets((prev) => [nweetObject, ...prev])
-        });
-    };
-
     useEffect(() => {
-        getNweets();
+        onSnapshot(collection(dbService, "nweets"), (snapshot) => {
+            const newArray = snapshot.docs.map((document) => ({
+                id: document.id,
+                ...document.data()
+            }));
+            setNweets(newArray);
+        });
     }, []);
 
     const onSubmit = async (event) => {
@@ -24,7 +23,7 @@ const Home = ({ userObj }) => {
             createdAt: Date.now(),
             creatorId: userObj.uid,
         });
-        setNweet("");
+        setNweets("");
     };
     const onChange = (event) => {
         event.preventDefault();
@@ -48,9 +47,11 @@ const Home = ({ userObj }) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet 
+                        key={nweet.id} 
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </>
